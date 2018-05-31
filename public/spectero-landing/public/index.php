25a50aa5e7e9cd5c29cddafa55fd0ae2171ed2ed
RobertClarke <robert@rjfc.net>
2018-05-30 18:42:40 -0700

    Update index.php

--- public/spectero-landing/public/index.php
+++ public/spectero-landing/public/index.php
@@ -22,7 +22,7 @@ include ('./includes/header.php');
           <h3>Spectero Daemon</h3>
         </header>
         <div class="info">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id malesuada ex, id tincidunt massa. Suspendisse eu ullamcorper arcu. Mauris eros risus, viverra dignissim cursus a, congue a mi. Nulla sem sapien, luctus a magna et, blandit sodales eros. Donec bibendum eros at odio venenatis fermentum ut et orci.</p>
+          <p>Utilizing a variety of VPN technologies such as OpenVPN, SSH and HAProxy our open source VPN server is an easy to use way of creating and maintaining your own VPN. Whether you're just looking to run a small home VPN connection or host a large enterprise proxy solution our powerful software is optimized for use cases large and small.</p>
           <a href="#" class="learn-more">Learn More</a>
         </div>
       </article>
