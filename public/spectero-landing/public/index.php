ca674b0f71d1e245f379402c69175b6a5141cc61
Anatolie Diordita <contact@anatolie.me>
2018-06-02 21:33:16 -0400

    Remove learn more links from homepage

--- public/spectero-landing/public/index.php
+++ public/spectero-landing/public/index.php
@@ -25,7 +25,7 @@ include ('./includes/header.php');
         </header>
         <div class="info">
           <p>Utilizing a variety of VPN technologies such as OpenVPN, SSH and HAProxy our open source VPN server is an easy to use way of creating and maintaining your own VPN. Whether you're just looking to run a small home VPN connection or host a large enterprise proxy solution our powerful software is optimized for use cases large and small.</p>
-          <a href="#" class="learn-more">Learn More</a>
+          <!-- <a href="#" class="learn-more">Learn More</a> -->
         </div>
       </article>
     </div>
@@ -40,7 +40,7 @@ include ('./includes/header.php');
         </header>
         <div class="info">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id malesuada ex, id tincidunt massa. Suspendisse eu ullamcorper arcu. Mauris eros risus, viverra dignissim cursus a, congue a mi. Nulla sem sapien, luctus a magna et, blandit sodales eros. Donec bibendum eros at odio venenatis fermentum ut et orci.</p>
-          <a href="#" class="learn-more">Learn More</a>
+          <!-- <a href="#" class="learn-more">Learn More</a> -->
         </div>
       </article>
     </div>
@@ -55,7 +55,7 @@ include ('./includes/header.php');
         </header>
         <div class="info">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id malesuada ex, id tincidunt massa. Suspendisse eu ullamcorper arcu. Mauris eros risus, viverra dignissim cursus a, congue a mi. Nulla sem sapien, luctus a magna et, blandit sodales eros. Donec bibendum eros at odio venenatis fermentum ut et orci.</p>
-          <a href="#" class="learn-more">Learn More</a>
+          <!-- <a href="#" class="learn-more">Learn More</a> -->
         </div>
       </article>
     </div>
@@ -70,7 +70,7 @@ include ('./includes/header.php');
         </header>
         <div class="info">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id malesuada ex, id tincidunt massa. Suspendisse eu ullamcorper arcu. Mauris eros risus, viverra dignissim cursus a, congue a mi. Nulla sem sapien, luctus a magna et, blandit sodales eros. Donec bibendum eros at odio venenatis fermentum ut et orci.</p>
-          <a href="#" class="learn-more">Learn More</a>
+          <!-- <a href="#" class="learn-more">Learn More</a> -->
         </div>
       </article>
     </div>
