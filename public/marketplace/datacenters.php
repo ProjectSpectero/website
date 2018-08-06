701f6174158015980e8ce0d87d6ef9b2800536de
Robert Clarke <robert@rjfc.net>
2018-08-06 15:38:56 -0700

    Spectero Marketplace for Datacenters page
    first revision

---
+++ public/marketplace/datacenters.php
@@ -0,0 +1,137 @@
+<?php
+$html_title = 'Spectero Marketplace for Datacenters';
+$header_h1 = 'Spectero Marketplace for Datacenters';
+$header_content = '<p>Spectero\'s marketplace solution for datacenters empowers businessses to generate revenue on their excess bandwidth. Tailored solutions such as GRE connectivity to our proxy servers enable VPN connectivity at scale.</p>';
+
+include ('../includes/header.php');
+?>
+<div id="managed">
+  <section class="body-white">
+    <div class="wrapper body-wrapper">
+      <div class="feature-columns columns-how-it-works">
+        <div class="col">
+          <h3>Automatic IP Rotation</h3>
+          <p>Our IP rotator employs a state of the art IP rotation system that allows full anonymity. The tool designed to minimize probability of IP based blocking, captcha implementation, throttling and false data delivery.</p>
+        </div>
+        <div class="col">
+          <h3>Powerful Dedicated Servers</h3>
+          <p>Our servers use enterprise grade hardware for optimal performance at all times. Every server runs custom-designed software and are based in reliable data centres across the world. We guarantee 99% uptime with our services.</p>
+        </div>
+        <div class="col">
+          <h3>Bank Grade Security</h3>
+          <p>The system blacklists IP addresses that have been already blocked or throttled, so they are not used again. It also automatically adds user strings into your queries, minimizing changes of getting blocked.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="body-alt">
+    <div class="wrapper body-wrapper centered">
+      <div class="section-title">
+        <h2>How It Works</h2>
+        <p class="section-description">Spectero Marketplace for Datacenters is powered by a state of the art global infrastructure.</p>
+      </div>
+      <div class="managed-diagram"></div>
+      <div class="managed-diagram-steps">
+        <article>
+          <p>First, your data is sent securely to our servers using enterprise grade AES encryption.</p>
+        </article>
+        <article>
+          <p>Your data then enters our entry nodes, where it is assigned a proxy to pass through.</p>
+        </article>
+        <article>
+          <p>Finally, your data passes through our global proxies, arriving anonymously at the target server.</p>
+        </article>
+      </div>
+    </div>
+  </section>
+  <section class="body-white">
+    <div class="wrapper body-wrapper">
+      <div class="section-title">
+        <h2>Fully Featured Proxies</h2>
+        <p class="section-description">Each proxy we host supports the following features.</p>
+      </div>
+      <div class="feature-columns columns-how-it-works">
+        <div class="col">
+          <h3>Advanced Monitoring</h3>
+          <p>Our advanced IP Rotator provides detailed statistics about all the queries you make. It allows you to monitor your usage and costs, enabling you to adjust them depending on your needs.</p>
+        </div>
+        <div class="col">
+          <h3>Full IPv6 Support</h3>
+          <p>IPv6 is perfect for data harvesting. We have implemented full IPv6 support so that you can benefit from the internet protocol of the future today.</p>
+        </div>
+        <div class="col">
+          <h3>Global Proxy Network</h3>
+          <p>Our proxies are based in multiple locations all over the world. This includes United States, Europe, and Asia, among others.</p>
+        </div>
+      </div>
+      <div class="faq">
+        <div class="questions">
+          <h4>Frequently Asked Questions</h4>
+          <ul>
+            <li data-answer-id="custom" class="selected">Can you prepare a custom proxy package for me?</li>
+            <li data-answer-id="blocked">We implement our own techniques to minimize exposure in our daily web scraping activities, but we still get blocked. What can we do about it?</li>
+            <li data-answer-id="type">What type of proxies do you have?</li>
+            <li data-answer-id="implement">Is it difficult to use Spectero Marketplace for Datacenters? Can it be implemented in my custom data mining tool?</li>
+          </ul>
+        </div>
+        <div class="answer">
+          <article data-answer-id="custom">
+            <h5>Can you prepare a custom proxy package for me?</h5>
+            <p>Yes, we can prepare any custom solution for your needs. Our IP network covers more than 100 locations and we work hard to expand our resource base every day. Just contact our sales team below.</p>
+          </article>
+          <article data-answer-id="blocked">
+            <h5>We implement our own techniques to minimize exposure in our daily web scraping activities, but we still get blocked. What can we do about it?</h5>
+            <p>Spectero Marketplace for Datacenters was built to tackle risks associated with data harvesting. IP blocking is just one of them, and even though it's the most common, it's also the easiest to solve. Our intelligent IP rotation algorithm minimizes the risk of IP-based blocking. Even if the IP is blacklisted by mistake, the site is still accessible through our other vastly scattered resources. The system was designed to deliver you the data, no matter what.</p>
+          </article>
+          <article data-answer-id="type">
+            <h5>What type of proxies do you have?</h5>
+            <p>We have high performance HTTP/HTTPS & Socks 4/5 proxies.</p>
+          </article>
+          <article data-answer-id="implement">
+            <h5>Is it difficult to use Spectero Marketplace for Datacenters? Can it be implemented in my custom data mining tool?</h5>
+            <p>Our core is intuitive and extremely easy to use. It's light and simple and can be implemented into any custom data harvesting script or tool.</p>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="contact" class="body-alt">
+    <div class="wrapper body-wrapper">
+      <div class="section-title">
+        <h2>Get In Touch</h2>
+        <p class="section-description">Do you offer datacenter services? Have a question about our service? Contact us below.</p>
+      </div>
+      <form id="contact-form">
+        <div class="input-3">
+          <div class="input">
+            <label for="contact-name">Full Name *</label>
+            <input type="text" name="name" id="contact-name" required maxlength="50">
+          </div>
+          <div class="input">
+            <label for="contact-email">Email Address *</label>
+            <input type="email" name="email" id="contact-email" required maxlength="100">
+          </div>
+          <div class="input">
+            <label for="contact-phone">Phone Number</label>
+            <input type="text" name="phone" id="contact-phone" maxlength="30">
+          </div>
+        </div>
+        <div class="input-3">
+          <div class="input">
+            <label for="contact-company">Company Name</label>
+            <input type="text" name="company" id="contact-company" maxlength="50">
+          </div>
+        </div>
+        <div class="input">
+          <label for="contact-message">Message *</label>
+          <textarea name="message" id="contact-message" placeholder="Please briefly describe the reason you’re contacting us and our team will be happy to assist you." required maxlength="1500" minlength="50"></textarea>
+        </div>
+        <div class="submit-button">
+          <button type="submit" class="button">Send Message</button>
+        </div>
+      </form>
+      <div id="contact-success"><p>Thank you for your interest.<br>Our sales team will be in touch within 24 hours.</p></div>
+    </div>
+  </section>
+</div>
+<?php include ('../includes/footer.php'); ?>
